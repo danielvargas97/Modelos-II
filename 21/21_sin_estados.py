0ed1585abbe1d6d64e92bc661e5a229c111ef773
Daniel Vargas <danielvargas999@gmail.com>
2017-08-10 20:42:56 -0500

    Se agrega funcion de revolver cartas y servir cartas a los jugadores al empezar el juego

--- 21/21_sin_estados.py
+++ 21/21_sin_estados.py
@@ -1,18 +1,49 @@
 # generador de mazo sin estados #
 # -*- coding:utf-8 -*- #
 
+from random import *
 
+
+#Establece las pintas disponibles#
 def pintas():
     return ["Diamante","Pica","Corazón","Trebol"]
 
+
+#Establece el nombre de las cartas#
 def nombre_carta():
     return["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
 
 
+#Crea un mazo con las cartas en orden#
 def crear_mazo(pinta,nombre):
     return [(x,y) for x in pinta for y in nombre]
 
 
+#Revuelve las cartas en el mazo#
+def revolver_mazo(mazo):
+    return sample(mazo,len(mazo))
+
+
+#Genera un mazo con cartas revueltas#
+def mazo():
+    return revolver_mazo(crear_mazo(pintas(),nombre_carta()))
+
+
+#Le da las cartas al jugador#
+def inicia_jugador(mazo):
+    return mazo[0:2]
+
+#Le da las cartas a la maquina#
+def inicia_com(mazo):
+    return mazo[2:4]
+
+
+#Inicia el juego#
+def iniciar_partida(mazo):
+    return [mazo[4:],inicia_jugador(mazo),inicia_com(mazo)]
 
 
 
+if __name__ == "__main__":
+    
+    print(iniciar_partida(mazo())[1:])
