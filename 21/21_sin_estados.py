c485e17c4f48ca091dce775214c4dadf346bc5f9
Daniel Vargas <danielvargas999@gmail.com>
2017-08-13 23:16:25 -0500

    Se rehace el código anterior y se agrega la opcion de pedir carta

--- 21/21_sin_estados.py
+++ 21/21_sin_estados.py
@@ -7,43 +7,110 @@ from random import *
 #Establece las pintas disponibles#
 def pintas():
     return ["Diamante","Pica","Corazón","Trebol"]
+def nombres():
+    return ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
 
-
-#Establece el nombre de las cartas#
-def nombre_carta():
-    return["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
-
-
-#Crea un mazo con las cartas en orden#
-def crear_mazo(pinta,nombre):
+def mazo(pinta,nombre):
     return [(x,y) for x in pinta for y in nombre]
 
-
-#Revuelve las cartas en el mazo#
-def revolver_mazo(mazo):
+def revolver(mazo): #Revuelve el mazo de cartas que tenemos# 
+    print("Revolviendo cartas...")
     return sample(mazo,len(mazo))
 
 
-#Genera un mazo con cartas revueltas#
-def mazo():
-    return revolver_mazo(crear_mazo(pintas(),nombre_carta()))
+def pide_carta(mano,mazo): #Pide una carta al dealer#
+    mano.append(mazo[0])
+    mazo.remove(mazo[0])
+
+    return mazo
+
+def valor_carta(carta): #Da un valor a la carta segun su nombre#
+    if carta[1] == 'J':
+        return 10
+    elif carta[1] == 'Q':
+        return 10
+    elif carta[1] == 'K':
+        return 10
+    elif carta[1] == 'A':
+        return 1
+    else:
+        return int(carta[1])
+"""
+def decide_as(suma): #Determina el valor del as#
+
+    if suma <= 10: #Si tengo 10, se asume como 11#
+        return 11
+    else: #El jugador solo puede asumir la carta como 1#
+        return 1
+"""
 
 
-#Le da las cartas al jugador#
-def inicia_jugador(mazo):
-    return mazo[0:2]
+def ver_mano_player(mano): #Imprime las cartas del jugador#
+    print("Tienes estas cartas: "+ str(mano))
 
-#Le da las cartas a la maquina#
-def inicia_com(mazo):
-    return mazo[2:4]
+def ver_mano_casa(mano): #Imprime las cartas del dealer#
+    print("La casa tiene: "+str(mano[0]))
+
+def repartir(mazo,dealer,player): #Reparte las cartas al principio de la partida#
+    print("Repartiendo las cartas")
+
+    player.append(mazo[0])
+    player.append(mazo[1])
+    dealer.append(mazo[2])
+    dealer.append(mazo[3])
+    return mazo[4:]
+
+def sumar_cartas(mano): #Toma cada carta y suma su valor#
+    if mano == []:
+        return 0
+    else:
+        return valor_carta(mano[0]) + sumar_cartas(mano[1:])
 
 
-#Inicia el juego#
-def iniciar_partida(mazo):
-    return [mazo[4:],inicia_jugador(mazo),inicia_com(mazo)]
+"""def jugar_maquina(mazo,mano):
+    if sumar_cartas(mano) >= 17:
+        return 
+    else:
+        return jugar_maquina(pide_carta(mazo,mano))
+
+def quitar_cartas(mano,mazo):
+    for x in mano:
+        mazo.remove(mazo[mazo.index(x)])
+    return mazo
+"""    
+
+def jugar(mazo,jugador,casa,estado,turno): #Empieza el juego de verdad#
+
+    if turno == 1: #Empieza el turno de la maquina#
+        print("El turno de la máquina comienza...")
 
 
+    if  turno == 0 and estado == 0: #Empezar la partida revolviendo el mazo#
+        jugar(revolver(mazo),jugador,casa,estado+1,turno)
+    elif turno == 0 and estado == 1: #Repartiendo las cartas#
+        jugar(repartir(mazo,casa,jugador),jugador,casa,estado+1,turno)
+    elif turno == 0:
+        print("Tu turno comienza...")
+        ver_mano_player(jugador)
+        print("Tienes:"+str(sumar_cartas(jugador)))
+        ver_mano_casa(casa)
+        
+        if(sumar_cartas(jugador) >21):
+            print("La casa GANA")
+            return False
+        else:
+            print("¿Pides otra carta?")
+            print("SI/NO (S/N)")
+            if input()== 'S':
+                jugar(pide_carta(jugador,mazo),jugador,casa,estado+1,turno)
+            else: #Aqui empieza a jugar la maquina#
+                jugar(mazo,jugador,casa,0,turno+1)
+    
 
 if __name__ == "__main__":
-    
-    print(iniciar_partida(mazo())[1:])
+    print()
+    print("21")
+    print()
+    jugar(mazo(pintas(),nombres()),[],[],0,0)
+
+
